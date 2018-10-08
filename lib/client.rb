0d0090b0034ea7e75d3bea09059305429d17a4f3
davidlawes <davidlawes@hotmail.co.uk>
2018-10-08 16:36:32 +0100

    style: refactored after running rubocop

--- lib/client.rb
+++ lib/client.rb
@@ -2,7 +2,8 @@ class Client
 
   attr_reader :balance, :transactions
 
-  def initialize(transaction_klass = Transaction, transaction_printer = TransactionPrinter)
+  def initialize(transaction_klass = Transaction,
+                  transaction_printer = TransactionPrinter)
     @transaction_klass = transaction_klass
     @transaction_printer = transaction_printer
     @balance = 0
@@ -11,14 +12,18 @@ class Client
 
   def deposit(amount, date)
     @balance += amount
-    transaction = @transaction_klass.new(date: date, credit: amount, balance: @balance)
+    transaction = @transaction_klass.new(date: date,
+                                          credit: amount,
+                                          balance: @balance)
     @transactions.push(transaction)
   end
 
   def withdraw(amount, date)
     insufficient_funds?(amount)
     @balance -= amount
-    transaction = @transaction_klass.new(date: date, debit: amount, balance: @balance)
+    transaction = @transaction_klass.new(date: date,
+                                          debit: amount,
+                                          balance: @balance)
     @transactions.push(transaction)
   end
 
--- lib/transaction_printer.rb
+++ lib/transaction_printer.rb
@@ -1,12 +1,11 @@
 module TransactionPrinter
+
   def self.pretty_print(transactions)
     'date || credit || debit || balance\n' + format_history(transactions)
   end
 
-  private
-
   def self.format_history(transactions)
-    transactions.reverse.each.map { | transaction |
+    transactions.reverse_each.map { |transaction|
       "#{format_date(transaction.date)} || "\
       "#{format_number(transaction.credit)} || "\
       "#{format_number(transaction.debit)} || "\
@@ -19,7 +18,11 @@ module TransactionPrinter
   end
 
   def self.format_number(number)
-    return if number == nil
+    return if number.nil?
+
     sprintf("%.2f", number)
   end
+
+  private_class_method :format_history, :format_date, :format_number
+
 end
