38fc321b1851d8093ede3e0cea0dd1376b5039c4
davidlawes <davidlawes@hotmail.co.uk>
2018-10-08 14:33:37 +0100

    test: failing feature test for client.print_statement

--- spec/features/print_transaction_history_feature_spec.rb
+++ spec/features/print_transaction_history_feature_spec.rb
@@ -2,6 +2,20 @@ describe 'Print Transaction History' do
 
   let(:client) { Client.new }
 
+  describe '#print_statement' do
+    before do
+      client.deposit(1000, '10-01-2012')
+      client.deposit(2000, '13-01-2012')
+      client.withdraw(500, '14-01-2012')
+    end
 
-
+    it 'pretty prints the transaction history' do
+      expect(client.print_statement).to eq(
+        'date || credit || debit || balance\n'\
+        '14/01/2012 || || 500.00 || 2500.00\n'\
+        '13/01/2012 || 2000.00 || || 3000.00\n'\
+        '10/01/2012 || 1000.00 || || 1000.00'
+      )
+    end
+  end
 end
