d896d95698d589dba6705c466cf54a0e4e21b623
davidlawes <davidlawes@hotmail.co.uk>
2018-10-08 16:20:22 +0100

    test: refactor feature tests to align with unit tests

--- spec/features/print_transaction_history_feature_spec.rb
+++ spec/features/print_transaction_history_feature_spec.rb
@@ -12,9 +12,9 @@ describe 'Print Transaction History' do
     it 'pretty prints the transaction history' do
       expect(client.print_statement).to eq(
         'date || credit || debit || balance\n'\
-        '14/01/2012 || || 500.00 || 2500.00\n'\
-        '13/01/2012 || 2000.00 || || 3000.00\n'\
-        '10/01/2012 || 1000.00 || || 1000.00'
+        '14/01/2012 ||  || 500.00 || 2500.00\n'\
+        '13/01/2012 || 2000.00 ||  || 3000.00\n'\
+        '10/01/2012 || 1000.00 ||  || 1000.00'
       )
     end
   end
