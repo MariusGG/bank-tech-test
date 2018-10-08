79e859ea3eacc5ce8436f9a612179162148a84cd
davidlawes <davidlawes@hotmail.co.uk>
2018-10-08 15:26:18 +0100

    feature: added format_date and format_history to TransactionPrinter

--- lib/transaction_printer.rb
+++ lib/transaction_printer.rb
@@ -1,4 +1,22 @@
 module TransactionPrinter
-  def self.pretty_print
+  def self.pretty_print(transactions)
+    header = 'date || credit || debit || balance\n'
+    format_date(transactions)
+    format_numbers(transactions)
+    return header + format_history(transactions)
+  end
+
+  private
+
+  def self.format_date(transactions)
+    transactions.each { | transaction |
+      transaction.date.gsub!(/([-])/, '/')
+    }
+  end
+
+  def self.format_history(transactions)
+    transactions.each.map { | transaction |
+      "#{transaction.date} || #{transaction.credit} || #{transaction.debit} || #{transaction.balance}"
+    }.join('\n')
   end
 end
--- spec/transaction_printer_spec.rb
+++ spec/transaction_printer_spec.rb
@@ -2,6 +2,23 @@ require 'transaction_printer'
 
 describe TransactionPrinter do
 
-  it { is_expected.to respond_to(:pretty_print) }
+  let(:transaction) { double :transaction, date: '10-01-2012', credit: 1000.00, debit: nil, balance: 1000.00 }
 
+  it { is_expected.to respond_to(:pretty_print).with(1).argument }
+
+  describe '#pretty_print' do
+
+    context 'given an array of transactions' do
+      before do
+        @transactions = [transaction]
+      end
+
+      it 'pretty prints the transaction history' do
+        expect(subject.pretty_print(@transactions)).to eq(
+          'date || credit || debit || balance\n'\
+          '10/01/2012 || 1000.00 ||  || 1000.00'
+        )
+      end
+    end
+  end
 end
