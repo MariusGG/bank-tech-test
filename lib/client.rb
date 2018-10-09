01f09061ac777442cd7275c9353de91720125e2d
davidlawes <davidlawes@hotmail.co.uk>
2018-10-09 10:38:04 +0100

    style: address offenses from linter

--- lib/client.rb
+++ lib/client.rb
@@ -14,15 +14,19 @@ class Client
 
   def deposit(money)
     raise 'Illegal tender!' if illegal_tender?(money)
+
     @balance += money
     @transactions.push(@transaction_klass.new(credit: money, balance: @balance))
+    return @balance
   end
 
   def withdraw(money)
     raise 'Illegal tender!' if illegal_tender?(money)
     raise 'Insufficient funds' if insufficient_funds?(money)
+    
     @balance -= money
     @transactions.push(@transaction_klass.new(debit: money, balance: @balance))
+    return @balance
   end
 
   def account_statement
